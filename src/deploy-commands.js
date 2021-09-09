8976ca0bb643642ad7c97a07a066be7c4ddb301f
Enis Mulić <enis.s.mulic@gmail.com>
2021-09-09 22:32:49 +0200

    chore(*): setup commands and events

---
+++ src/deploy-commands.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const fs = require("fs");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+
+const {
+  discord: { token, clientId }
+} = require("./app.development.json");
+
+const commands = [];
+fs.readdirSync(path.resolve(__dirname, "commands")).forEach((dir) => {
+  const commandFiles = fs
+    .readdirSync(path.resolve(__dirname, `commands/${dir}`))
+    .filter((files) => files.endsWith(".js"));
+
+  for (const file of commandFiles) {
+    const command = require(`./commands/${dir}/${file}`);
+    commands.push(command.data.toJSON());
+  }
+});
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+(async () => {
+  try {
+    await rest.put(Routes.applicationCommands(clientId), {
+      body: commands
+    });
+
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
