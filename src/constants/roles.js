633e4327354feaa15c07b46a31ac8bfde15d7668
Enis Mulić <enis.s.mulic@gmail.com>
2021-09-13 21:58:34 +0200

    feat(commands): add echo command, closes #20

--- src/constants/roles.js
+++ src/constants/roles.js
@@ -1,1 +1,4 @@
+module.exports.ADMINISTRATOR = "Administrator";
+module.exports.MODERATOR = "Moderator";
 module.exports.NEREGISTROVAN = "Neregistrovan(a)";
+module.exports.EVERYONE = "@everyone";
--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -2,6 +2,7 @@ const path = require("path");
 const fs = require("fs");
 const { REST } = require("@discordjs/rest");
 const { Routes } = require("discord-api-types/v9");
+const { Client, Intents } = require("discord.js");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -14,7 +15,7 @@ fs.readdirSync(path.resolve(__dirname, "commands")).forEach((dir) => {
 
   for (const file of commandFiles) {
     const command = require(`./commands/${dir}/${file}`);
-    commands.push(command.data.toJSON());
+    commands.push(command);
   }
 });
 
@@ -23,13 +24,51 @@ const rest = new REST({ version: "9" }).setToken(
 );
 
 (async () => {
+  const client = new Client({
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MEMBERS]
+  });
+
+  await client.login(process.env.DISCORD_CLIENT_TOKEN);
+
   try {
-    await rest.put(Routes.applicationCommands(process.env.DISCORD_CLIENT_ID), {
-      body: commands
-    });
+    const deployedCommands = await rest.put(
+      Routes.applicationCommands(process.env.DISCORD_CLIENT_ID),
+      {
+        body: commands.map((command) => command.data.toJSON())
+      }
+    );
+
+    const permissionCommands = commands.filter(
+      (command) => command.getPermissions != null
+    );
+
+    const permissionList = await Promise.all(
+      permissionCommands.map(async (command) => {
+        const deployedCommand = deployedCommands.find(
+          (cmd) => command.data.name === cmd.name
+        );
+
+        const permissions = await command.getPermissions(client);
+
+        return {
+          id: deployedCommand.id,
+          permissions: [...permissions]
+        };
+      })
+    );
+
+    await rest.put(
+      Routes.guildApplicationCommandsPermissions(
+        process.env.DISCORD_CLIENT_ID,
+        process.env.DISCORD_GUILD_ID
+      ),
+      { body: permissionList }
+    );
 
     console.log("Successfully registered application commands.");
   } catch (error) {
     console.error(error);
   }
+
+  client.destroy();
 })();
