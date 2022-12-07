42a5602aea425e0288fa6dd3bc6f5387ea30b6d8
Ilham <ilham.krhan@gmail.com>
2022-12-07 22:42:06 +0100

    poruka

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,9 +2,11 @@ import { ISlashCommand } from "./command";
 import * as miscCommands from "./misc";
 import * as rankupCommands from "./rankup";
 
+
 const commands: ISlashCommand[] = [
   miscCommands.echoCommand,
   miscCommands.inviteCommand,
+  miscCommands.mutedCommand,
   rankupCommands.apsolventCommand,
   rankupCommands.cistaCommand,
   rankupCommands.diplomaCommand,
