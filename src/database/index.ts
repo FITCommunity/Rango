9f0a2493b5692b5903f84e6e5018a84d9425b453
Enis Mulić <enis.s.mulic@gmail.com>
2022-12-10 14:22:39 +0100

    chore: use connection string

--- src/database/index.ts
+++ src/database/index.ts
@@ -3,24 +3,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DATABASE_NAME as string,
-  process.env.DATABASE_USER as string,
-  process.env.DATABASE_PASSWORD as string,
-  {
-    host: process.env.DATABASE_HOST as string,
-    dialect: "mysql",
-    logging: false,
-    ssl: true,
-    define: {
-      timestamps: false
-    },
-    dialectOptions: {
-      ssl: {
-        rejectUnauthorized: true
-      }
-    }
-  }
-);
+// const sequelize = new Sequelize(
+//   process.env.DATABASE_NAME as string,
+//   process.env.DATABASE_USER as string,
+//   process.env.DATABASE_PASSWORD as string,
+//   {
+//     host: process.env.DATABASE_HOST as string,
+//     dialect: "mysql",
+//     logging: false,
+//     ssl: true,
+//     define: {
+//       timestamps: false
+//     },
+//     dialectOptions: {
+//       ssl: {
+//         rejectUnauthorized: true
+//       }
+//     }
+//   }
+// );
+
+const sequelize = new Sequelize(process.env.DATABASE_URL as string);
 
 export default sequelize;
